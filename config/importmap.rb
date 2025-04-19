6b53d4b29c0448cd8d10f49afaf4c0a52ab382fc
yokikoshimizu <yokikoshimizu@gmail.com>
2025-04-19 09:25:14 +0900

    ToDoのプルリク用コミットです！

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "@rails/ujs", to: "rails-ujs.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "bootstrap" # @5.3.5
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
